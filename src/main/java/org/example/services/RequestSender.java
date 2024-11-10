0653c6365ea85a972eb36d8a05c7482053f954ff
ecaterina <ecaterinaciobanu49@gmail.com>
2024-11-10 13:20:09 +0200

    Added all methodds to RequestSeder

--- src/main/java/org/example/services/RequestSender.java
+++ src/main/java/org/example/services/RequestSender.java
@@ -175,4 +175,198 @@ public class RequestSender {
         }
     }
 
+    public static Transaction addNewTransaction(Transaction transaction) {
+        try {
+            String requestBody = objectMapper.writeValueAsString(transaction);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .uri(URI.create(BASE_URL + POST_TRANSACTION))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return objectMapper.readValue(response.body(), Transaction.class);
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    public static List<Transaction> getAllTransactionByAccountId(Long accountId) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create(BASE_URL + GET_TRANSACTION_BY_ACCOUNT + accountId))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return objectMapper.readValue(response.body(), new TypeReference<List<Transaction>>() {});
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    public static Transaction getTransactionById(Long transactionId) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create(BASE_URL + GET_TRANSACTION_BY_ID + transactionId))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return objectMapper.readValue(response.body(), Transaction.class);
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    public static Loan addNewLoan(Loan loan) {
+        try {
+            String requestBody = objectMapper.writeValueAsString(loan);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .uri(URI.create(BASE_URL + POST_LOAN))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return objectMapper.readValue(response.body(), Loan.class);
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    public static List<Loan> retrieveLoansForACustomer(String subjectCode) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create(BASE_URL + GET_LOAN_BY_CUSTOMER + subjectCode))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return objectMapper.readValue(response.body(), new TypeReference<List<Loan>>() {});
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    public static Loan getLoanById(Long loanId) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create(BASE_URL + GET_LOAN_BY_ID + loanId))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return objectMapper.readValue(response.body(), Loan.class);
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    public static Loan updateOutstandingAmount(Long loanId, Double balance) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .PUT(HttpRequest.BodyPublishers.ofString(String.valueOf(balance)))
+                    .uri(URI.create(BASE_URL + PUT_LOAN_AMOUNT + loanId))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return objectMapper.readValue(response.body(), Loan.class);
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    public static boolean closeLoan(Long loanId) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .PUT(HttpRequest.BodyPublishers.noBody())
+                    .uri(URI.create(BASE_URL + PUT_LOAN_CLOSE + loanId))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return true;
+        } catch (IOException | InterruptedException ignored) {
+            return false;
+        }
+    }
+
+    public static Card addNewCard(Card card) {
+        try {
+            String requestBody = objectMapper.writeValueAsString(card);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .uri(URI.create(BASE_URL + POST_CARD))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return objectMapper.readValue(response.body(), Card.class);
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    public static List<Card> getCardsBySubjectCode(String subjectCode) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create(BASE_URL + GET_CARDS_BY_CUSTOMER + subjectCode))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return objectMapper.readValue(response.body(), new TypeReference<List<Card>>() {});
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    public static Card getCardById(Long cardId) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create(BASE_URL + GET_CARD_BY_ID + cardId))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return objectMapper.readValue(response.body(), Card.class);
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+    }
+
+
 }
